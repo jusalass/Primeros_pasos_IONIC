3617211ecef9ddf55b34f51087d775ece5866395
Vina del Mar <vina@duoc.acad>
2025-08-21 19:24:00 -0400

    estructura de login funcional con enrutamiento por código login.page.ts

--- primeros_pasos/src/app/app.component.ts
+++ primeros_pasos/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet,IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet,IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonTitle],
 })
 export class AppComponent {
   constructor() {}
--- primeros_pasos/src/app/login/login.page.ts
+++ primeros_pasos/src/app/login/login.page.ts
@@ -1,23 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonItem, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton   } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonToolbar, IonInput, IonItem, IonList, IonCard, IonButton, IonTitle   } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonInput, IonItem, IonList, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton   ]
+  imports: [IonContent, IonHeader, IonToolbar, CommonModule, FormsModule, IonInput, IonItem, IonList, IonCard, IonButton,IonTitle  ]
 })
 export class LoginPage implements OnInit {
 
-  mail: string = "";
-  pass: string = "";
+  mail: string = '';
+  pass: string = '';
+  isToastOpen: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+  }
+
+  login() {
+    console.log(this.mail);
+    console.log(this.pass);
+
+    if (this.mail === 'jesus.vargas@tinet.cl' && this.pass === '123456') {
+      this.router.navigateByUrl('/home');
+    } else {
+      this.isToastOpen = true;
+    }
   }
 
 }
